3067f2afc8a9249007a436d0e37ce77d3606cc8f
ewqscxz078 <ewqscxz078@gmail.com>
2023-04-25 07:44:26 +0800

    udpate init

--- 1_0_init_format.sh
+++ 1_0_init_format.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
-func_2unix(){
+func_check_is_window(){
+  # 使用「od」指令來查看檔案的十六進位表示，看是否包含「0d 0a」（即「CR+LF」）
+  # od -c filename.txt | grep -E "\\r|\\n"
+  return `find . -type f -name "*.txt" -print0 | xargs -0 od -c | grep -l -E "\\r|\\n"`
+}
 
+func_check_is_window_by_file(){
   # 使用 file ${filepath} 判斷
   # dos : filename.txt: ASCII text, with CRLF line terminators
   # unix : filename.txt: ASCII text
-  # 使用「od」指令來查看檔案的十六進位表示，看是否包含「0d 0a」（即「CR+LF」）
-  # od -c filename.txt
-  # find -type f -name "*.txt" | xargs dos2unix
-  #echo $result
-  #result=`find . -type f -name "*.txt"`
-  # 不使用 file 執行後符合的結果回傳，僅輔助判定，不然會有中文路徑或檔名解譯問題
-  #listDos2unix=()
-  #for aa in $result; do
-  #  if file $aa | grep -q "CRLF"; then
-#	   紀錄要處理 dos2unix
-  #	  echo "需要處理的 $aa"
-  #  listDos2unix+=($aa)
-#	fi
-#  done
-  #for target in ${listDos2unix[@]}; do
-    #dos2unix $target
-  #done
+  return `find . -type f -name "*.txt" -print0 | xargs -0 file | grep "CRLF" | awk -F ":" '{print $1}'`
+  #return `find . -type f -name "*.txt" -print0 | xargs -0 file | grep -l "CRLF"`
+}
 
-  # TODO 在有中文路徑的下的 window 換行格式會有問題
+func_2unix(){
+  # 在有中文路徑的下的 window 換行格式輸出若為 ascii 會有問題，代表當前編輯器輸出的格式非預期
   result=`find . -type f -name "*.txt" -print0 | xargs -0 file | grep "CRLF" | awk -F ":" '{print $1}'`
   for target in $result; do
     dos2unix $target
